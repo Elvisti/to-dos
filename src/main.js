fd0e5bab006e25d50865aafd756b0980c357067a
Elvis Chagas <elvischagas123@gmail.com>
2019-12-21 23:27:16 -0300

    commit pagina main. Criando elementos com JS

---
+++ src/main.js
@@ -0,0 +1,10 @@
+let formEl = document.getElementById('container');
+let inputEl = document.getElementById('txtList');
+let ulEl = document.getElementById('list');
+
+let todos = [];
+
+for(todo of todos){
+    let liElement = document.createElement('li');
+    let text = document.createTextNode('todo');
+}
